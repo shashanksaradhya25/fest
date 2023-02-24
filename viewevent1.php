b92f8f91b4b1fd6bd4f040a96f5ffc9c8c3189c9
shashanksaradhya25 <110121269+shashanksaradhya25@users.noreply.github.com>
2023-02-24 13:11:34 +0530

    Add files via upload

---
+++ viewevent1.php
@@ -0,0 +1,121 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href=css/main.css rel="stylesheet" type="text/css">
+<link href=css/home.css rel="stylesheet" type="text/css">
+<title>PESITM</title>
+<style>
+.button {
+	
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+table{
+border-collapse: collapse;
+width:100%;
+color:#588c7e;
+text-align:center;
+}
+th{
+background-color:#588c7e;
+color: white;
+}
+tr:nth-child(even){
+background-color:#f2f2f2;}
+h2{
+padding-top:30px;
+text-align:center ;
+text-decoration:underline;
+}
+.content_table{
+padding-left:20px;
+padding-right:20px;
+}
+
+</style>
+
+</head>
+
+<body>
+<div class="body">
+
+
+
+<div class="content_table">
+<?php
+
+	$conn = mysqli_connect("localhost","root","","fest");
+	if ($conn-> connect_error)
+	{
+	die("connection failed:".$conn-> connect_error);
+	}
+	
+	$sql = "SELECT w.eid as eid, w.usn as usn, w.place as place, w.price as price, e.name as name from win as w,eventt as e where w.eid=e.eid order by place";
+	$result = $conn-> query($sql);
+
+	
+	
+	
+	
+	if($result-> num_rows >0)
+	{
+	?>
+	<div >
+	<h2>Win Details</h2>
+	</div>
+	<table cellpadding="8px" border="3px soild">
+	<tr>
+	<th>eid </th>
+	<th>usn</th>
+	<th>place</th>	
+	<th>price</th>
+	<th>name</th>
+	</tr>
+	
+	<?php
+	
+		while ($row = $result-> fetch_assoc() )
+		{	
+			$i=$row["eid" ];
+			
+			echo"<tr>";
+			echo"<td>";echo $row["eid"]; echo"</td>";
+			echo"<td>";echo $row["usn" ]; echo"</td>";
+			echo"<td>";echo $row["place"]; echo"</td>";
+			echo"<td>";echo $row["price"]; echo"</td>";
+			echo"<td>";echo $row["name"]; echo"</td>";
+			;
+		
+
+		}
+		echo"</table>";
+	}
+	else
+	{
+		echo"No Records found !";
+		
+	}
+	$conn-> close();
+		
+	?>
+	
+</table>
+</div>
+</div>
+
+</body>
+</html>
